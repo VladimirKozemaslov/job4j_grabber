701476cda52998955be210f4a999f5505ae668d0
VladimirKozemaslov <kvs@intelins.ru>
2022-12-20 11:45:53 +0500

    2.2. Модель данных - Post. [#285211 #372724]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -10,8 +10,51 @@ public class Post {
     private String description;
     private final LocalDateTime created;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
     public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
         this.link = link;
         this.description = description;
@@ -27,23 +70,19 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description) && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, created);
+        return Objects.hash(id, link);
     }
 
     @Override
     public String toString() {
         return "Post{"
                 + "id=" + id
-                + ", title='" + title + '\''
                 + ", link='" + link + '\''
-                + ", description='" + description
-                + '\''
-                + ", created=" + created
                 + '}';
     }
 }
